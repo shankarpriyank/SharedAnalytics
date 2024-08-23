64fde232a4b3a30176c95157f14665412166a53a
Priyank Shankar <shankarpriyank312002@gmail.com>
2024-08-23 18:38:14 +0530

    will this work? finally????

--- shared/src/iosMain/kotlin/com/priyank/sharedanalytics/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/priyank/sharedanalytics/Platform.ios.kt
@@ -2,8 +2,9 @@ package com.priyank.sharedanalytics
 
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+class IOSPlatform : Platform {
+    override val name: String =
+        UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.model() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
