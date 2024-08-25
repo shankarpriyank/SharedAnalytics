a70fa9925e159ebbe611433381c3dfc36291d60d
Priyank Shankar <shankarpriyank312002@gmail.com>
2024-08-25 18:57:22 +0530

    Add device name

--- shared/src/androidMain/kotlin/com/priyank/sharedanalytics/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/priyank/sharedanalytics/Platform.android.kt
@@ -1,7 +1,7 @@
   package com.priyank.sharedanalytics
 
 class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+    override val name: String = "Android  ${android.os.Build.DEVICE} ${android.os.Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
